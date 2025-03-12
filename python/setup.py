c0e5c892054cccdce3b3fa13276163c444a0784d
Cvikli <havlikmarcell@gmail.com>
2025-03-12 17:35:50 +0100

    julia any python folder deleted

--- python/setup.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-from setuptools import setup
-
-# This file is here for backward compatibility with older pip versions
-# Modern installations will use pyproject.toml instead
-setup()
