19693f1ffe17770c86d07fa473cc7081354e9837
SixZero <havliktomi@hotmail.com>
2025-05-20 08:47:49 +0200

    Removing build warnings.

--- edge_frontend/src/components/dashboard/Dashboard.tsx
+++ edge_frontend/src/components/dashboard/Dashboard.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import WSMessageViewer from './WSMessageViewer';
 import WorkspacePathsList from './WorkspacePathsList';
 import SyncedFilesList from './SyncedFilesList';
-import { Typography, Grid } from '@mui/material';
+import { Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';  // Grid v2
 import UserMenu from '../header/UserMenu';
 
 export const Dashboard: React.FC = () => {
@@ -15,13 +16,13 @@ export const Dashboard: React.FC = () => {
       </Typography>
 
       <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
+        <Grid size={{ xs: 12, md: 6 }}>
           <WorkspacePathsList />
         </Grid>
-        <Grid item xs={12}>
+        <Grid size={12}>
           <SyncedFilesList />
         </Grid>
-        <Grid item xs={12}>
+        <Grid size={12}>
           <WSMessageViewer />
         </Grid>
       </Grid>
