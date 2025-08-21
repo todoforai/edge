ec88c05f41ee91e211afbb779b1098679953ae3d
Cvikli <havlikmarcell@gmail.com>
2025-08-21 13:31:13 +0200

    RMEOVING UNUSED files

--- edge_frontend/src/utils/mcpDataConverter.ts
+++
@@ -1,6 +0,0 @@
-import type { MCPEdgeExecutable } from '../shared/REST_types_shared';
-
-// No conversion needed - just use MCPEdgeExecutable directly
-export const getMCPInstances = (instances: MCPEdgeExecutable[]): MCPEdgeExecutable[] => {
-  return instances;
-};
