8a50434f35c3ffde5193d2a481dec31880b44514
Cvikli <havlikmarcell@gmail.com>
2026-09-07 13:56:36 +0200

    Revert "edge: answer GET /identity ({deviceId}) on the loopback http server, mirroring bridge/identity_server.c"
    This reverts commit 2e10da5cf3ebdfd15a2b0e113a7a6a2b54da2276.

--- bun/src/edge.ts
+++ bun/src/edge.ts
@@ -107,7 +107,6 @@ export class TODOforAIEdge {
     this.mayflyWorkspace = config.mayflyWorkspace;
     this.addWorkspacePath = config.addWorkspacePath;
     this.browserExtensionBridge = new BrowserExtensionBridge(this.debug);
-    this.browserExtensionBridge.identity = () => this.edgeId && !this.mayflyTodoId ? this.edgeId : null;
     setConnectionContext(() => ({ apiUrl: this.api.apiUrl, sessionToken: this.sessionToken }));
   }
 
